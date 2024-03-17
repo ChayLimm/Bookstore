341e02c3191416be29c89191d9a57b68ba9892ab
Sovanda <bansovanda.net@gmail.com>
2024-03-17 15:28:17 +0700

    update khmerbook

--- khmerbook.php
+++ khmerbook.php
@@ -1,34 +1,133 @@
 <?php
-  include 'config.php'
+    include 'config.php'
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
 
-  <title>Book Category</title>
+    <title>Book Category</title>
+
+    <style>
+        img {
+            border: none;
+        }
+
+        .message {
+            position: sticky;
+            top: 0;
+            margin: 0 auto;
+            width: 61%;
+            background-color: #fff;
+            padding: 6px 9px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            z-index: 100;
+            gap: 0px;
+            border: 2px solid rgb(68, 203, 236);
+            border-top-right-radius: 8px;
+            border-bottom-left-radius: 8px;
+        }
+
+        .message span {
+            font-size: 22px;
+            color: rgb(240, 18, 18);
+            font-weight: 400;
+        }
+
+        .message i {
+            cursor: pointer;
+            color: rgb(3, 227, 235);
+            font-size: 15px;
+        }
+    </style>
 </head>
 
 <body>
   
-      <?php include 'index_header.php' ?>
-<!-- 
-      <div class="box-container">
+    <?php include 'index_header.php' ?>
+
+    <?php
+    if (isset($message)) {
+        foreach ($message as $message) {
+            echo '
+        <div class="message" id= "messages"><span>' . $message . '</span>
+        </div>
+        ';
+        }
+    }
+    ?>
+
+    <section id="KhmerBook">
+
+        <div class="container px-5 mx-auto">
+            <h2 class="text-gray-400 m-8 font-extrabold text-4xl text-center border-t-2 text-red-800"
+                style="color: rgb(0, 167, 245);">
+                Khmer Book
+            </h2>
+        </div>
+
+        </section>
+        <section class="show-products">
+        <div class="box-container">
 
             <?php
-            // $select_book = mysqli_query($conn, "SELECT * FROM `book_info` where category='Khmer Book' ") or die('query failed');
-            // if (mysqli_num_rows($select_book) > 0) {
-               
-            // } else {
-            //     echo '<p class="empty" style="margin: auto">No products added yet!</p>';
-            // }
-            ?>
-        </div> -->
+            $select_book = mysqli_query($conn, "SELECT * FROM `book_info` Where category='KhmerBook' LIMIT 4") or die('query failed');
+            if (mysqli_num_rows($select_book) > 0) {
+                while ($fetch_book = mysqli_fetch_assoc($select_book)) {
+                    ?>
 
-      <?php include 'index_footer.php'; ?>
+                    <div class="box" style="width: 255px;height: 355px;">
+                        <a href="book_details.php?details=<?php echo $fetch_book['bid'];
+                        echo '-name=', $fetch_book['name']; ?>"> <img
+                                style="height: 200px;width: 125px;margin: auto;" class="books_images"
+                                src="added_books/<?php echo $fetch_book['image']; ?>" alt=""></a>
+                        <div style="text-align:left ;">
+
+                            <div style="font-weight: 500; font-size:18px; text-align: center;" class="name">
+                                <?php echo $fetch_book['name']; ?>
+                            </div>
+                        </div>
+                        <div class="price">Price: $
+                            <?php echo $fetch_book['price']; ?>
+                        </div>
+                        <!-- <button name="add_cart"><img src="./images/cart2.png" alt=""></button> -->
+                        <form action="" method="POST">
+                            <input class="hidden_input" type="hidden" name="book_name"
+                                value="<?php echo $fetch_book['name'] ?>">
+                            <input class="hidden_input" type="hidden" name="book_image"
+                                value="<?php echo $fetch_book['image'] ?>">
+                            <input class="hidden_input" type="hidden" name="book_price"
+                                value="<?php echo $fetch_book['price'] ?>">
+                            <button name="add_to_cart"><img src="./images/cart2.png" alt="Add to cart">
+                                <a href="book_details.php?details=<?php echo $fetch_book['bid'];
+                                echo '-name=', $fetch_book['name']; ?>"
+                                    class="update_btn">Know More</a>
+                        </form>
+                        <!-- <button name="add_to_cart" ><img src="./images/cart2.png" alt="Add to cart"></button> -->
+                        <!-- <input type="submit" name="add_cart" value="cart"> -->
+                    </div>
+                    <?php
+                }
+                echo '<p class="empty" style="margin: auto; color: blue; font-size:18px;">More Book Coming Soon...!</p>';
+
+            } else {
+                echo '<p class="empty" style="margin: auto; color: red">No products added yet!</p>';
+            }
+            
+            ?>
+        </div>
+    </section>
+
+    <?php include 'index_footer.php'; ?>
   
 </body>
 </html>
